2341f21fe6ea8538b87d4ab2d711a883c434e182
navin-morajkar <navinsika@gmail.com>
2024-09-18 17:59:07 +0530

    syntax

--- EmployeeAdminPortal/Data/ApplicationDbContext.cs
+++ EmployeeAdminPortal/Data/ApplicationDbContext.cs
@@ -6,8 +6,7 @@ namespace EmployeeAdminPortal.Data
     public class ApplicationDbContext :DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base (options)
-        {
-            
+        {           
         }
 
         public DbSet<Employee> Employees { get; set; }
